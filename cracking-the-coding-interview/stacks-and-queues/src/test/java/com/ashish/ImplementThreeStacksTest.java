5d74ee381194f6f868dfc68890aee252e7d946e7
ashiishsharma <ashiish.sharma@gmail.com>
2016-09-09 20:08:09 +0530

    Adding code

--- cracking-the-coding-interview/stacks-and-queues/src/test/java/com/ashish/ImplementThreeStacksTest.java
+++ cracking-the-coding-interview/stacks-and-queues/src/test/java/com/ashish/ImplementThreeStacksTest.java
@@ -9,12 +9,49 @@ import org.testng.annotations.Test;
  */
 public class ImplementThreeStacksTest {
 
+    int stackSize = 100;
+    int[] buffer = new int[stackSize * 3];
+    int[] stackPointer = {-1, -1, -1}; // pointers to track top element
+
     @BeforeMethod
-    public void init(){
+    public void init() {
     }
 
     @Test
-    public void implementThreeStacks(){
+    public void implementThreeStacks() {
+    }
 
+    void push(int stackNum, int value) throws Exception {
+         /* Check if we have space */
+        if (stackPointer[stackNum] + 1 >= stackSize) { // Last element
+            throw new Exception("0ut of space.");
+        }
+         /* Increment stack pointer and then update top value */
+        stackPointer[stackNum]++;
+        buffer[absTopOfStack(stackNum)] = value;
+    }
+
+    int pop(int stackNum) throws Exception {
+        if (stackPointer[stackNum] == -1) {
+            throw new Exception("Trying to pop an empty stack.");
+        }
+        int value = buffer[absTopOfStack(stackNum)]; // Get top
+        buffer[absTopOfStack(stackNum)] = 0; // Clear index
+        stackPointer[stackNum]--; // Decrement pointer
+        return value;
+    }
+
+    int peek(int stackNum) {
+        int index = absTopOfStack(stackNum);
+        return buffer[index];
+    }
+
+    boolean isEmpty(int stackNum) {
+        return stackPointer[stackNum] == -1;
+    }
+
+    /* returns index of top of stack "stackNum", in absolute terms */
+    int absTopOfStack(int stackNum) {
+        return stackNum * stackSize + stackPointer[stackNum];
     }
 }
