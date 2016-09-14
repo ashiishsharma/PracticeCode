5d2bac8f2d00e3dc56b7d78426024bd7b08571a6
ashiishsharma <ashiish.sharma@gmail.com>
2016-09-15 00:30:39 +0530

    Added code

---
+++ cracking-the-coding-interview/stacks-and-queues/src/main/java/com/ashish/StackWithMinimum.java
@@ -0,0 +1,10 @@
+package com.ashish;
+
+/**
+ * Created by Ashish Sharma on 13-09-2016.
+ */
+public class StackWithMinimum extends Stack {
+
+    public StackWithMinimum(){
+    }
+}
---
+++ cracking-the-coding-interview/stacks-and-queues/src/test/java/com/ashish/MinimumStackElementTest.java
@@ -0,0 +1,25 @@
+package com.ashish;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * 3.2
+ * How would you design a stack which, in addition to push and pop, also has a
+ * function min which returns the minimum element? Push, pop and min should all
+ * operate in 0(1) time.
+ *
+ * Created by Ashish Sharma on 13-09-2016.
+ */
+public class MinimumStackElementTest {
+
+    @BeforeMethod
+    public void init() {
+
+    }
+
+    @Test
+    public void stackWithMinimum(){
+
+    }
+}
