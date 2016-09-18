d0dcfdda933eaf48bbd7fa3236d657b2bc1a0d34
ashiishsharma <ashiish.sharma@gmail.com>
2016-09-18 23:17:01 +0530

    Added code

---
+++ cracking-the-coding-interview/stacks-and-queues/src/test/java/com/ashish/SetOfStacksTest.java
@@ -0,0 +1,33 @@
+package com.ashish;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * 3.3
+ * Imagine a (literal) stack of plates. If the stack gets too high, it might topple.
+ * Therefore, in real life, we would likely start a new stack when the previous
+ * stack exceeds some threshold.
+ * Implement a data structure SetOfStacks that mimics this. SetOf-Stacks should
+ * be composed of several stacks and should create a new stack once
+ * the previous one exceeds capacity. SetOfStacks.push() and SetOfStacks.
+ * pop () should behave identically to a single stack (that is, pop () should return the
+ * same values as it would if there were just a single stack).
+ * *FOLLOW UP
+ * Implement a function popAt(int index) which performs a pop operation on a
+ * specific sub-stack.
+ * <p>
+ * Created by Ashish Sharma on 18-09-2016.
+ */
+public class SetOfStacksTest {
+
+    @BeforeMethod
+    public void init() {
+
+    }
+
+    @Test
+    public void testSetOfStacks() {
+
+    }
+}
