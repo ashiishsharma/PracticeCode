6cfda4630af3e2f16b97bb3f6817040c1beffb44
ashiishsharma <ashiish.sharma@gmail.com>
2016-09-18 21:28:44 +0530

    Added code

--- cracking-the-coding-interview/stacks-and-queues/src/main/java/com/ashish/StackWithMinimum.java
+++ cracking-the-coding-interview/stacks-and-queues/src/main/java/com/ashish/StackWithMinimum.java
@@ -1,10 +1,43 @@
 package com.ashish;
 
+import java.util.Comparator;
+import java.util.EmptyStackException;
+
 /**
  * Created by Ashish Sharma on 13-09-2016.
  */
-public class StackWithMinimum extends Stack {
+public class StackWithMinimum<T extends Comparable> extends Stack<T> {
 
-    public StackWithMinimum(){
+    Stack<T> minimumElementStack;
+
+    public StackWithMinimum() {
+        minimumElementStack = new Stack<T>();
+    }
+
+    public void push(T t) {
+        if (min() != null && t.compareTo(min()) <= 0) {
+            minimumElementStack.push(t);
+        } else if (min() == null) {
+            minimumElementStack.push(t);
+        }
+        super.push(t);
+    }
+
+    public T pop() {
+        T t = super.pop();
+        if (min() != null && t.compareTo(min()) == 0) {
+            minimumElementStack.pop();
+        }
+        return t;
+    }
+
+    public T min() {
+        T t = null;
+        try {
+            t = minimumElementStack.peek();
+        } catch (EmptyStackException e) {
+            System.out.println("Minimum element stack empty");
+        }
+        return t;
     }
 }
--- cracking-the-coding-interview/stacks-and-queues/src/test/java/com/ashish/MinimumStackElementTest.java
+++ cracking-the-coding-interview/stacks-and-queues/src/test/java/com/ashish/MinimumStackElementTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
  * How would you design a stack which, in addition to push and pop, also has a
  * function min which returns the minimum element? Push, pop and min should all
  * operate in 0(1) time.
- *
+ * <p>
  * Created by Ashish Sharma on 13-09-2016.
  */
 public class MinimumStackElementTest {
@@ -19,7 +19,18 @@ public class MinimumStackElementTest {
     }
 
     @Test
-    public void stackWithMinimum(){
+    public void stackWithMinimum() {
 
+        StackWithMinimum stackWithMinimum = new StackWithMinimum();
+        stackWithMinimum.push(3);
+        System.out.println(stackWithMinimum.min());
+        stackWithMinimum.push(56);
+        stackWithMinimum.push(34);
+        System.out.println(stackWithMinimum.min());
+        stackWithMinimum.push(1);
+        System.out.println(stackWithMinimum.min());
+        System.out.println(stackWithMinimum.pop());
+        System.out.println(stackWithMinimum.pop());
+        System.out.println(stackWithMinimum.min());
     }
 }
