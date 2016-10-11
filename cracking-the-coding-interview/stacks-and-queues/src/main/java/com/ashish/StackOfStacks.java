e105971c6b34390ddf84e9c218cad9b5d429949f
ashiishsharma <ashiish.sharma@gmail.com>
2016-10-12 01:33:00 +0530

    Added code

---
+++ cracking-the-coding-interview/stacks-and-queues/src/main/java/com/ashish/StackOfStacks.java
@@ -0,0 +1,53 @@
+package com.ashish;
+
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.List;
+
+/**
+ * Created by Ashish Sharma on 11-10-2016.
+ */
+public class StackOfStacks<T> {
+
+    private List<T> stackArr;
+    private int elementCount;
+
+    public StackOfStacks() {
+        stackArr = new ArrayList<T>();
+        elementCount = 0;
+    }
+
+    public List<T> getStackArr() {
+        return stackArr;
+    }
+
+    public void push(T obj) {
+        stackArr.add(obj);
+        elementCount++;
+    }
+
+    public T pop() {
+        T element = null;
+        if (elementCount > 0) {
+            element = stackArr.remove(elementCount - 1);
+            elementCount--;
+        } else {
+            throw new EmptyStackException();
+        }
+        return element;
+    }
+
+    public T peek() {
+        T element = null;
+        if (elementCount > 0) {
+            element = stackArr.get(elementCount - 1);
+        } else {
+            throw new EmptyStackException();
+        }
+        return element;
+    }
+
+    public int stackLength() {
+        return elementCount;
+    }
+}
---
+++ cracking-the-coding-interview/stacks-and-queues/src/test/java/com/ashish/TowersOfHanoiTest.java
@@ -0,0 +1,20 @@
+package com.ashish;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Ashish Sharma on 12-10-2016.
+ */
+public class TowersOfHanoiTest {
+
+    @BeforeMethod
+    public void init(){
+
+    }
+
+    @Test
+    public void towerOfHanoiTest(){
+
+    }
+}
