7385f464fe1f8cc27ea4c1143a7b495d71946807
ashiishsharma <ashiish.sharma@gmail.com>
2016-12-30 17:43:57 +0530

    Adding code for problem

---
+++ cracking-the-coding-interview/stacks-and-queues/src/test/java/com/ashish/QueueUsingTwoStacksTest.java
@@ -0,0 +1,18 @@
+package com.ashish;
+
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * 3.5
+ * <p>
+ * Implement a MyQueue class which implements a queue using two stacks.
+ *
+ * Answer Reference :
+ * http://stackoverflow.com/questions/69192/how-to-implement-a-queue-using-two-stacks
+ * <p>
+ *
+ * @author Ashish Sharma on 10/18/2016.
+ */
+public class QueueUsingTwoStacksTest {
+
+}
