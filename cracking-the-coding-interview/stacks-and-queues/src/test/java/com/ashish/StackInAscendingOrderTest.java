288b26f24f45ae668b13f59b43ee39f6b83437b5
ashiishsharma <ashiish.sharma@gmail.com>
2017-01-11 00:26:19 +0530

    Adding more solution

---
+++ cracking-the-coding-interview/stacks-and-queues/src/test/java/com/ashish/StackInAscendingOrderTest.java
@@ -0,0 +1,18 @@
+package com.ashish;
+
+/**
+ * 3.6
+ * <p>
+ * Write a program to sort a stack in ascending order (with biggest items on top).
+ * You may use at most one additional stack to hold items, but you may not copy the
+ * elements into any other data structure (such as an array). The stack supports the
+ * following operations: push, pop, peek, and isEmpty.
+ * <p>
+ *
+ * @author Ashish Sharma on 1/5/2017.
+ * @since 1/5/2017
+ */
+public class StackInAscendingOrderTest {
+
+
+}
