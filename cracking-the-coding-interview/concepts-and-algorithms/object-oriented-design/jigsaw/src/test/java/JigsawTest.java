d34426c53fe1c45c82071fce04e22efa2fbaa3c9
ashiish-sharma <ashiish.sharma@gmail.com>
2019-01-04 15:28:01 +0530

    Adding jigsaw solution.

---
+++ cracking-the-coding-interview/concepts-and-algorithms/object-oriented-design/jigsaw/src/test/java/JigsawTest.java
@@ -0,0 +1,29 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * 8.6
+ * <p>
+ * <B>Jigsaw</B>: Implement an NxN jigsaw puzzle. Design the data structures and explain an algorithm to
+ * solve the puzzle. You can assume that you have a fi tsWi th method which, when passed two
+ * puzzle edges, returns true if the two edges belong together.
+ * <p>
+ *
+ * <p> The solution contains the thought process of classes that need to be
+ * there rest of the full code is skipped </p>
+ *
+ * <p>Refer book itself for more detailed solution, since it has no fixed solution</p>
+ *
+ * @author Ashish Sharma on 1/3/2019.
+ * <p>
+ * @since 1/3/2019.
+ */
+
+public class JigsawTest {
+
+    @BeforeMethod
+    public void initialize(){
+
+    }
+
+}
