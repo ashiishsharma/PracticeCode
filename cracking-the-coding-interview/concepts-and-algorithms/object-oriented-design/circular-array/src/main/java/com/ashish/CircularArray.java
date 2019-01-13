4b68086ff8ac2274ac438a6e5668f8943bb71be0
ashiishsharma <ashiish.sharma@gmail.com>
2019-01-13 20:39:07 +0530

    Adding circular array solution

--- cracking-the-coding-interview/concepts-and-algorithms/object-oriented-design/circular-array/src/main/java/com/ashish/CircularArray.java
+++ cracking-the-coding-interview/concepts-and-algorithms/object-oriented-design/circular-array/src/main/java/com/ashish/CircularArray.java
@@ -1,6 +1,8 @@
 package com.ashish;
 
-public class CircularArray<T> {
+import java.util.Iterator;
+
+public class CircularArray<T> implements Iterable<T> {
     private T circularArray[];
     private int size;
     private int headIndexLocation;
@@ -12,22 +14,59 @@ public class CircularArray<T> {
     }
 
     public void set(int index, T object) {
-        if (index < size) {
-            circularArray[index] = object;
-        } else {
-            throw new ArrayIndexOutOfBoundsException();
-        }
+        circularArray[rotatedIndex(index)] = object;
     }
 
     public T get(int index) {
-        if (index < size) {
-            return circularArray[index];
+        if (index < size && index >= 0) {
+            return circularArray[rotatedIndex(index)];
         } else {
             throw new ArrayIndexOutOfBoundsException();
         }
     }
 
+    private int rotatedIndex(int index) {
+        if (index < 0) {
+            index = index + size;
+        }
+        int indexLocation = (headIndexLocation + index) % size;
+        return indexLocation;
+    }
+
     public void rotateRight(int shiftRight) {
-            headIndexLocation = shiftRight%size;
+        headIndexLocation = rotatedIndex(shiftRight);
+    }
+
+    public Iterator<T> iterator() {
+        return new CircularArrayIterator<T>(this);
+    }
+
+    private class CircularArrayIterator<TI> implements Iterator<TI> {
+        /* current reflects the offset from the rotated head, not from the actual
+         * start of the raw array. */
+        private int current = -1;
+
+        private TI[] items;
+
+        public CircularArrayIterator(CircularArray<TI> array) {
+            items = array.circularArray;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current < circularArray.length - 1;
+        }
+
+        @Override
+        public TI next() {
+            current++;
+            TI item = (TI) items[rotatedIndex(current)];
+            return item;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Not supported ...");
+        }
     }
 }
--- cracking-the-coding-interview/concepts-and-algorithms/object-oriented-design/circular-array/src/test/java/com/ashish/CircularArrayTest.java
+++ cracking-the-coding-interview/concepts-and-algorithms/object-oriented-design/circular-array/src/test/java/com/ashish/CircularArrayTest.java
@@ -1,11 +1,14 @@
 package com.ashish;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
- * 8.9
+ * 7.9
+ *
+ * <p>From Cracking the coding interview 6th edition</p>
  *
  * <p><B>Circular Array</B>: Implement a CircularArray class that supports an array-like data structure which
  * can be efficiently rotated.</p>
@@ -22,22 +25,41 @@ import org.testng.annotations.Test;
  * @since 1/4/2019.
  */
 public class CircularArrayTest {
-    private CircularArray circularArray;
+    private CircularArray<Integer> circularArray;
 
     @BeforeMethod
     public void initialize() {
         circularArray = new CircularArray<Integer>(10);
+        circularArray.set(0, 0);
+        circularArray.set(1, 1);
+        circularArray.set(2, 2);
+        circularArray.set(3, 3);
+        circularArray.set(4, 4);
+        circularArray.set(5, 5);
+        circularArray.set(6, 6);
+        circularArray.set(7, 7);
+        circularArray.set(8, 8);
+        circularArray.set(9, 9);
+
     }
 
     @Test
-    public void testArrayOperation(){
-        circularArray.set(2,4);
-        Assert.assertEquals(4,circularArray.get(2));
+    public void testArrayOperation() {
+        circularArray.set(2, 4);
+        Assert.assertEquals(4, (Object) circularArray.get(2));
         circularArray.rotateRight(5);
     }
 
     @Test
+    public void testArrayIteration() {
+        circularArray.rotateRight(5);
+        for (Object i : circularArray) {
+            System.out.println("Array objects : " + i);
+        }
+    }
+
+    @Test
     public void testModulo() {
-        System.out.println(-5 % -3);
+        System.out.println(-4 % 8);
     }
 }
