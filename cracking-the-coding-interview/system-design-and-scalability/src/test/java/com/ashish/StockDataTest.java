57eb3817c79759baeb74e55c771dab011a163fcb
ashiish-sharma <ashiish.sharma@gmail.com>
2019-03-07 20:10:51 +0530

    Adding more problem solutions.

---
+++ cracking-the-coding-interview/system-design-and-scalability/src/test/java/com/ashish/StockDataTest.java
@@ -0,0 +1,25 @@
+package com.ashish;
+
+/**
+ * <B>9.1</B> ,  <B>Cracking The Coding Interview : Edition : 6</B>
+ * <p>
+ * <B>Stock Data:</B>
+ * <p>
+ * Imagine you are building some sort of service that will be called by up to 1,000 client
+ * applications to get simple end-of-day stock price information (open, close, high, low).
+ * <p>
+ * You may assume that you already have the data, and you can store it in any format you wish.
+ * <p>
+ * How would you design the client-facing service that provides the information to client applications?
+ * You are responsible for the development, rollout, and ongoing monitoring and maintenance of the feed.
+ * <p>
+ * Describe the different methods you considered and why you would recommend your approach.
+ * <p>
+ * Your service can use any technologies you wish, and can distribute the information to the client applications in
+ * any mechanism you choose.
+ *
+ * @author Ashish Sharma on 03/07/2019.
+ * @since 03/07/2019.
+ */
+public class StockDataTest {
+}
